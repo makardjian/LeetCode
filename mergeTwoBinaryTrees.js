9b236a476c5e793ffdc73829c982494ca528e099
Michael Kardjian <makardjian@gmail.com>
2019-03-27 11:20:02 -0700

    changed mergeTwoBinaryTrees.js to use the proper recursive solution

--- mergeTwoBinaryTrees.js
+++ mergeTwoBinaryTrees.js
@@ -24,55 +24,14 @@ Merged tree:
 	 5   4   7
 */
 
-var mergeTrees = function(t1, t2) {
-  let t1Values = [];
-  let t2Values = [];
-  let result = [];
-  
-  const breadthFirstSearch = (root, arr) => {
-    if (!root) return;
-    let current;
-    let queue = [root];
-    
-    while (queue.length) {
-      current = queue.shift();
-      if (current !== null) {
-        arr.push(current.val);
-        queue.push(current.left, current.right);
-      } else {
-        arr.push(null);
-      }
-    }
-    for (let i = arr.length - 1; i >= 0; i -= 1) {
-      if (arr[i] === null) arr.pop();
-      else break;
-    }
-  } 
-  breadthFirstSearch(t1, t1Values);
-  breadthFirstSearch(t2, t2Values);
-  
-  let biggerArray = t1Values;
-  let smallerArray = t2Values;
-  if (t2Values.length > t1Values.length) {
-    biggerArray = t2Values;
-    smallerArray = t1Values;
-  }
-  
-  for (let i = 0; i < smallerArray.length; i += 1) {
-    if (biggerArray[i]  && smallerArray[i]) {
-      result.push(biggerArray[i] + smallerArray[i])
-    } else {
-      result.push(biggerArray[i] || smallerArray[i]);
-    }
-  }
-  
-  let remainingValues = biggerArray.slice(smallerArray.length);
-  result = result.concat(remainingValues);
-  return result; 
-}
 
-/*
-Note: I'm returning the array of values that would be in the binary tree.
-Proper solution would have to declare a BinaryTree class equiped with an
-insert method. Then call that method on each value in the result array.
-*/
+var mergeTrees = function(t1, t2) {
+  if (!t1) return t2;
+  if (!t2) return t1;
+  
+  t1.val = t1.val + t2.val;
+  t1.left = mergeTrees(t1.left, t2.left);
+  t1.right = mergeTrees(t1.right, t2.right);
+
+  return t1;
+}
