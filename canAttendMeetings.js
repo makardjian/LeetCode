173c68c70ea84da8b93629f5a67fd29264b0310f
Michael Kardjian <makardjian@gmail.com>
2019-11-29 11:54:37 -0800

    reformat canAttendMeetings

--- canAttendMeetings.js
+++ canAttendMeetings.js
@@ -1,16 +1,18 @@
-var canAttendMeetings = function(intervals) {
+const canAttendMeetings = (intervals) => {
   intervals.sort((a, b) => a.start - b.start);
 
   for (let i = 1; i < intervals.length; i += 1) {
-      let prev = intervals[i - 1];
-      let current = intervals[i];
-      if (prev.end > current.start) return false;
+    const prev = intervals[i - 1];
+    const current = intervals[i];
+    if (prev.end > current.start) return false;
   }
-  return true; 
-}
+  return true;
+};
 
-var data = [
-{start: 190, end: 200},
-{start: 30, end: 45},
-{start: 50, end: 60}
-]
+const data = [
+  { start: 190, end: 200 },
+  { start: 30, end: 45 },
+  { start: 50, end: 60 },
+];
+
+
